632873a17147130461cbea6d8eacdff0ebe85f56
Okpechi Chibuikem Michael <mikeokpechi@gmail.com>
2022-03-13 10:46:58 +0100

    working but bad structure

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const request = require("request");//remember to install request(npm i request)
 const app= express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-  var amountOfCourses,num;
+  var amountOfCourses,num,number;
+
 app.get("/", function(req,res){
   res.sendFile(__dirname+"/index.html");
 
@@ -18,50 +19,45 @@ app.post("/",function(req,res){
   console.log(amountOfCourses);
 
   res.sendFile(__dirname+"/simulate.html");
-document.querySelector("h1.result").style.visibility="visible";
+// document.querySelector("h1.result").style.visibility="visible";
 
 });
 app.post("/simulate",function(req,res){
-  res.sendFile(__dirname+"/simulate.html");
-  number=amountOfCourses;
-    num=parseInt(num);
-    num-=1;
-    var i=0;
-    while(i<13)
-    {
-      if(i<=num){
-
-        document.querySelectorAll("ul")[i].classList.remove("visibility");
-      }
-
-        if(i>num){
-
-          document.querySelectorAll("ul")[i].classList.add("visibility");
-        }
-  i++;
-  }
+  // res.sendFile(__dirname+"/simulate.html");
+  number=amountOfCourses;//AMOUNT OF COURSES USER HAS
 
 
 
 
-
+//number array
   var names=[];
   var grades=[];
   var units=[];
   var points=[];
   var sum=0;
     var score=[];
+  var i;
+//char array
+  var Sname =[req.body.name1,req.body.name2,req.body.name3,req.body.name4,req.body.name5,req.body.name6,req.body.name7,req.body.name8,
+  req.body.name9,req.body.name10,req.body.name11,req.body.name12];
+  var Sgrade=[ req.body.grade1,req.body.grade2,req.body.grade3,req.body.grade4,req.body.grade5,req.body.grade6,req.body.grade7,
+    req.body.grade8,req.body.grade9,req.body.grade10,req.body.grade11,req.body.grade12];
+    var Sunit=[ req.body.unit1,req.body.unit2,req.body.unit3,req.body.unit4,req.body.unit5,req.body.unit6,req.body.unit7,req.body.unit8,
+    req.body.unit9,req.body.unit10,req.body.unit11,req.body.unit12 ];
+
 
     for(i=0;i<number;i++)
     {
-      var name =document.querySelectorAll(".name")[i].value;
+      // var name =req.body
+
+       var name =Sname[i];
       names.push( name);
     }
   //grade is aphabet
 
     for(i=0;i<number;i++)
     {
-      var grade= document.querySelectorAll(".grade")[i].value;
+      var grade= Sgrade[i];
       grade=parseFloat(grade);
 
       if(grade<=100 && grade>=80)
@@ -88,7 +84,7 @@ app.post("/simulate",function(req,res){
     //stop
     for(i=0;i<number;i++)
     {
-      var grade= document.querySelectorAll(".grade")[i].value;
+      var grade= Sgrade[i];
       grade=parseFloat(grade);
 
       if(grade<=100 && grade>=80)
@@ -114,7 +110,7 @@ app.post("/simulate",function(req,res){
     }
     for(i=0;i<number;i++)
     {
-      var unit = document.querySelectorAll(".unit")[i].value;
+      var unit = Sunit[i];
       unit=parseFloat(unit);
       units.push(unit);
     }
@@ -135,11 +131,12 @@ app.post("/simulate",function(req,res){
 
     }
     var average = sum/tunits;
-    document.querySelector("h1.result").style.visibility="visible";
+    // document.querySelector("h1.result").style.visibility="visible";
 
 
+    
+    res.send("GPA ="+average);
 
-    document.querySelectorAll("h1.result")[0].textContent="GPA: "+ average;
 });
 app.listen(process.env.PORT||3000,function(){
   console.log("server is running");
